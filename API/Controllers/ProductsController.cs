5b590315794286f15b74ea2895e17db11b9393f3
Andries Bingani <andries.bingani@entelect.co.za>
2023-02-09 00:17:42 +0200

    added product brands and types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -9,19 +10,21 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() {
-            return await _context.Products.ToListAsync();
-        }
+        public async Task<ActionResult<List<Product>>> GetProducts() => Ok(await _repo.GetProductsAsync());
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) {
-            return await _context.Products.FindAsync(id);
-        }
+        public async Task<ActionResult<Product>> GetProduct(int id) => await _repo.GetProductByIdAsync(id);
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes() => Ok(await _repo.GetProductTypesAsync());
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands() => Ok(await _repo.GetProductBrandsAsync());
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,5 @@
 # My Awesome Online shop
 ## Running Api
 run dotnet program `dotnet watch --no-hot-reload`
+to add migrations `dotnet ef migrations add InitialCreate -p Infrastructure -s API -o Data/Migrations`
+to run migrations `dotnet ef database update -p Infrastructure -s API`
