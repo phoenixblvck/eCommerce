8fbd8f8b899b51efa5c86eac2191afaf3924e6b1
Andries Bingani <andries.bingani@entelect.co.za>
2023-02-12 10:47:55 +0200

    adding some pazzazz to the app

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -12,19 +13,24 @@ export class ProductDetailsComponent implements OnInit {
   product?: Product;
   constructor(
     private shopService: ShopService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
   ) {}
 
   ngOnInit(): void {
+    this.bcService.set('@productDetails',' ')
     this.loadProduct();
   }
 
   loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
-    if(id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
-      error: error => console.log(error)
-    });
-
+    if (id)
+      this.shopService.getProduct(+id).subscribe({
+        next: (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        error: (error) => console.log(error),
+      });
   }
 }
