935fd36d6733af3235029b46df4a7b0562a30348
Andries Bingani <andries.bingani@entelect.co.za>
2023-02-21 17:24:05 +0200

    added basked and functionality to client side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,11 @@ import { Product } from './shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Tranquil';
-  products: Product[] = [];
 
-  constructor() {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId) this.basketService.getBasket(basketId);
   }
 }
